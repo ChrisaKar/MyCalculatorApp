4a20cbd8f3e91b1865b9dc9fc457c85b0cfd7c8a
ChrisaKar <72608803+ChrisaKar@users.noreply.github.com>
2023-12-07 11:23:28 +0200

    Add files via upload

--- app/src/main/java/com/unipi/chrisakar/mycalculatorapp/CalculatorFunction.kt
+++ app/src/main/java/com/unipi/chrisakar/mycalculatorapp/CalculatorFunction.kt
@@ -1,7 +1,10 @@
 package com.unipi.chrisakar.mycalculatorapp
 
+import android.util.Log
+import android.widget.Toast
+
 class CalculatorFunction {
-    var operand1 = "0";
+    var operand1 = "";
     var operand2 = "";
     var operator = ' '
     var lastIsDecimalPoint = false;
@@ -10,14 +13,12 @@ class CalculatorFunction {
 
     fun String.fullTrim() = trim().replace("\uFEFF", "")
 
-    fun operatorFromChar(charOperator: Char):(Double, Double)->Double
-    {
-        return when(charOperator)
-        {
-            '+'->{a,b->a+b}
-            '-'->{a,b->a-b}
-            '/'->{a,b->a/b}
-            '*'->{a,b->a*b}
+    fun operatorFromChar(charOperator: Char): (Double, Double) -> Double {
+        return when (charOperator) {
+            '+' -> { a, b -> a + b }
+            '-' -> { a, b -> a - b }
+            '/' -> { a, b -> a / b }
+            '*' -> { a, b -> a * b }
             else -> throw Exception("That's not a supported operator")
         }
     }
@@ -25,122 +26,136 @@ class CalculatorFunction {
     fun CheckLastInput(CurrentInput: Char): String {
         when (CurrentInput) {
             '.' -> {
-                if (operand2 != "") {
-                    if (!operand2.contains('.')) {
-                        operand2 += '.'
-                        totalText += '.'
-                    }
-                } else if (operand1 != "" && !lastIsOperator) {
-                    if (!operand1.contains('.')) {
-                        operand1 += '.'
-                        totalText += '.'
-                    }
-                } else {
-                    operand1 += "0."
-                    totalText += "0."
-                }
-                lastIsOperator = false
-                lastIsDecimalPoint = true
+                handleDecimalPoint()
             }
             '+', '-', '*', '/' -> {
-
-                if (lastIsDecimalPoint) {
-                    totalText = totalText.dropLast(1)
-                    if (operand2 != "") {
-                        operand2 = operand2.dropLast(1)
-                    } else if (operand1 != "") {
-                        operand1 = operand1.dropLast(1)
-                    }
-                }
-
-                if (operand1 != "" && operand2 != "") {
-                    if (operand2.fullTrim().toDouble() == 0.0 && operator == '/') {
-                        // error here
-                    } else {
-                        val number = operatorFromChar(operator).invoke(operand1.fullTrim().toDouble(), operand2.fullTrim().toDouble())
-                        operand1 = number.toString()
-                        operand2 = ""
-                        operator = CurrentInput
-                        if (number == number.toInt().toDouble()) {
-                            operand1 = number.toInt().toString()
-                        }
-                        totalText = operand1 + operator
-                    }
-                } else if (operand1 != "") {
-                    operator = CurrentInput
-                    totalText = operand1 + operator
-                }
-                lastIsOperator = true
-                lastIsDecimalPoint = false
+                handleOperators (CurrentInput)
             }
             '=' -> {
-                if (operand1 != "" && operand2 != "" && operator != ' ') {
-                    if (lastIsDecimalPoint) {
-                        totalText = totalText.dropLast(1)
-                        if (operand2 != "") {
-                            operand2 = operand2.dropLast(1)
-                        } else if (operand1 != "") {
-                            operand1 = operand1.dropLast(1)
-                        }
-                    }
-
-                    if (operand2.fullTrim().toDouble() == 0.0 && operator == '/') {
-                        // error here
-                    } else {
-
-                        val number = operatorFromChar(operator).invoke(operand1.fullTrim().toDouble(), operand2.fullTrim().toDouble())
-
-                        operand1 = number.toString()
-                        operand2 = ""
-                        operator = ' '
-                        if (number == number.toInt().toDouble()) {
-                            operand1 = number.toInt().toString()
-                        }
-                        totalText = operand1
-                    }
-                }
-
+                handleEquals()
             }
             '~' -> {
-                totalText = totalText.dropLast(1);
-                if (operand2 != "") {
-                    operand2 = operand2.dropLast(1)
-                } else if (lastIsOperator) {
-                    lastIsOperator = false
-                    operator = ' '
-                } else if (lastIsDecimalPoint) {
-                    lastIsDecimalPoint = false
-                } else if (operand1 != "") {
-                    operand1 = operand1.dropLast(1)
-                }
-                if (!totalText.equals("")){
-               when (totalText.last()) {
-                   '+',  '-', '*', '/' -> {
-                       lastIsOperator = true
-                       operator = totalText.last()
-               } '.' -> {
-                   lastIsDecimalPoint = true
-               }
-               } }
+                handleBackspace()
 
             }
             else -> {
+                handleDigit(CurrentInput)
 
-                totalText += CurrentInput
-
-                if (operand2 != "" || (lastIsOperator && operand1 != "")) {
-                    operand2 += CurrentInput
-                } else {
-                    operand1 += CurrentInput
-                }
-                lastIsDecimalPoint = false
-                lastIsOperator = false;
             }
 
         }
         return totalText
     }
+
+    private fun handleEquals() {
+        if (operand1 != "" && operand2 != "" && operator != ' ') {
+
+            decimalPointBeforeInput();
+            performCalculation(' ')
+        }
+    }
+
+    private fun handleOperators (CurrentInput: Char) {
+
+        decimalPointBeforeInput();
+
+        if (operand1 != "" && operand2 != "") {
+            Log.d("here", "here" + operand1 + operand2)
+            performCalculation(CurrentInput)
+        } else if (operand1 != "") {
+            Log.d("here", "else if")
+            operator = CurrentInput
+            totalText = operand1 + operator
+        }
+        lastIsOperator = true
+    }
+
+    private fun handleDecimalPoint () {
+        if (operand2 != "") {
+            if (!operand2.contains('.')) {
+                operand2 += '.'
+                totalText += '.'
+            }
+        } else if (operand1 != "" && !lastIsOperator) {
+            if (!operand1.contains('.')) {
+                operand1 += '.'
+                totalText += '.'
+            }
+        } else {
+            operand1 += "0."
+            totalText += "0."
+        }
+        lastIsOperator = false
+        lastIsDecimalPoint = true
+    }
+
+    private fun handleBackspace () {
+        totalText = totalText.dropLast(1);
+        if (operand2 != "") {
+            operand2 = operand2.dropLast(1)
+        } else if (lastIsOperator) {
+            lastIsOperator = false
+            operator = ' '
+        } else if (lastIsDecimalPoint) {
+            lastIsDecimalPoint = false
+        } else if (operand1 != "") {
+            operand1 = operand1.dropLast(1)
+        }
+        if (!totalText.equals("")) {
+            when (totalText.last()) {
+                '+', '-', '*', '/' -> {
+                    lastIsOperator = true
+                    operator = totalText.last()
+                }
+                '.' -> {
+                    lastIsDecimalPoint = true
+                }
+            }
+        }
+    }
+
+    private fun handleDigit (CurrentInput: Char) {
+        totalText += CurrentInput
+
+        if (operand2 != "" || (lastIsOperator && operand1 != "")) {
+            operand2 += CurrentInput
+        } else {
+            operand1 += CurrentInput
+        }
+        lastIsDecimalPoint = false
+        lastIsOperator = false;
+    }
+
+    private fun decimalPointBeforeInput() {
+
+        if (lastIsDecimalPoint) {
+            totalText = totalText.dropLast(1)
+            if (operand2 != "") {
+                operand2 = operand2.dropLast(1)
+            } else if (operand1 != "") {
+                operand1 = operand1.dropLast(1)
+            }
+            lastIsDecimalPoint = false
+        }
+    }
+
+    private fun performCalculation (CurrentOperator: Char){
+
+        if (operand2.fullTrim().toDouble() == 0.0 && operator == '/') {
+            // error here
+        } else {
+
+            val number = operatorFromChar(operator).invoke(operand1.fullTrim().toDouble(), operand2.fullTrim().toDouble())
+
+            operand1 = number.toString()
+            operand2 = ""
+            operator = CurrentOperator
+            if (number == number.toInt().toDouble()) {
+                operand1 = number.toInt().toString()
+            }
+            totalText = (operand1 + CurrentOperator).fullTrim()
+        }
+    }
+
 }
-
-
 
